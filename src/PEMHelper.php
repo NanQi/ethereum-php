31d4cbaec800b4d83731686783c4130bbd9c70d1
nanqi <luyu_1216@126.com>
2019-07-02 18:12:36 +0800

    添加遗漏文件；

---
+++ src/PEMHelper.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ * author: NanQi
+ * datetime: 2019/7/2 10:06
+ */
+
+namespace Ethereum;
+
+use Sop\CryptoTypes\Asymmetric\EC\ECPrivateKey;
+use Sop\CryptoEncoding\PEM;
+
+use InvalidArgumentException;
+use RuntimeException;
+use Elliptic\EC;
+
+
+class PEMHelper
+{
+    /**
+     * Generate a new Private / Public key pair
+     * 
+     * @return string
+     */
+    public static function generateNewPrivateKey()
+    {
+
+        $config = [
+            'private_key_type' => OPENSSL_KEYTYPE_EC,
+            'curve_name' => 'secp256k1'
+        ];
+
+        $res = openssl_pkey_new($config);
+        if (!$res) {
+            throw new RuntimeException(
+                'ERROR: Failed to generate private key. -> ' . openssl_error_string()
+            );
+        }
+
+        // Generate Private Key
+        openssl_pkey_export($res, $priv_key);
+
+        // Get The Public Key
+        $priv_pem = PEM::fromString($priv_key);
+
+        // Convert to Elliptic Curve Private Key Format
+        $ec_priv_key = ECPrivateKey::fromPEM($priv_pem);
+
+        // Then convert it to ASN1 Structure
+        $ec_priv_seq = $ec_priv_key->toASN1();
+
+        // Private Key & Public Key in HEX
+        $priv_key_hex = bin2hex($ec_priv_seq->at(1)->asOctetString()->string());
+
+        return $priv_key_hex;
+    }
+
+    /**
+     * Generate the Address of the provided Public key
+     * 
+     * @param string $publicKey 
+     * 
+     * @return string
+     */
+    public static function publicKeyToAddress(string $publicKey)
+    {
+        if (Utils::isHex($publicKey) === false) {
+            throw new InvalidArgumentException('Invalid public key format.');
+        }
+        $publicKey = Utils::stripZero($publicKey);
+        if (strlen($publicKey) !== 130) {
+            throw new InvalidArgumentException('Invalid public key length.');
+        }
+        return '0x' . substr(Utils::sha3(substr(hex2bin($publicKey), 1)), 24);
+    }
+
+    /**
+     * Generate the Address of the provided Private key
+     * 
+     * @param string $privateKey 
+     * 
+     * @return string
+     */
+    public static function privateKeyToAddress(string $privateKey)
+    {
+        return self::publicKeyToAddress(
+            self::privateKeyToPublicKey($privateKey)
+        );
+    }
+
+    /**
+     * Generate the Public key for provided Private key
+     * 
+     * @param string $privateKey Private Key
+     * 
+     * @return string
+     */
+    public static function privateKeyToPublicKey( string $privateKey )
+    {
+        if (Utils::isHex($privateKey) === false) {
+            throw new InvalidArgumentException('Invalid private key format.');
+        }
+        $privateKey = Utils::stripZero($privateKey);
+
+        if (strlen($privateKey) !== 64) {
+            throw new InvalidArgumentException('Invalid private key length.');
+        }
+
+        $secp256k1 = new EC('secp256k1');
+        $privateKey = $secp256k1->keyFromPrivate($privateKey, 'hex');
+        $publicKey = $privateKey->getPublic(false, 'hex');
+
+        return '0x' . $publicKey;
+    }
+}
---
+++ tests/WalletTest.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * This file is part of the PHPEthereumTools package
+ *
+ * PHP Version 7.1
+ * 
+ * @category PHPEthereumTools
+ * @package  PHPEthereumTools
+ * @author   Micky Socaci <micky@nowlive.ro>
+ * @license  https://github.com/mickys/php-eth-tools/blob/master/LICENSE.md MIT
+ * @link     https://github.com/mickys/php-eth-tools/
+ */
+namespace Tests;
+
+
+use BIP\BIP44;
+use Ethereum\Wallet;
+use Ethereum\PEMHelper;
+use FurqanSiddiqui\BIP39\BIP39;
+use PHPUnit\Framework\TestCase as BaseTestCase;
+
+class WalletTest extends BaseTestCase
+{
+    function testGenerateANewEthereumPrivateKey()
+    {
+        $privateKey = PEMHelper::generateNewPrivateKey();
+        $publicKey = PEMHelper::privateKeyToPublicKey($privateKey);
+        $addressFromPrivate = PEMHelper::privateKeyToAddress($privateKey);
+
+        echo "  Private Key: ".$privateKey."\n";
+        echo "  Public Key:  ".$publicKey."\n";
+        echo "  Address:     ".$addressFromPrivate."\n\n";
+
+        $this->assertTrue(true);
+    }
+
+    public function testBIP44()
+    {
+        $mnemonic = BIP39::Words("glue country blast dash license flat weasel whip organ fun piano crazy");
+        $seed = $mnemonic->generateSeed();
+
+        $HDKey = BIP44::fromMasterSeed($seed)->derive("m/44'/60'/0'/0/0");
+
+        $privateKey = $HDKey->privateKey;
+        $this->assertEquals("e438f6c9d13dcc80734d2b22a73967359e11e9a1e0352501d62d0192c24e9f19", $privateKey);
+
+    }
+
+    public function testNewAccount()
+    {
+        $res1 = Wallet::newAccountByPrivateKey();
+        var_dump($res1);
+
+        $res2 = Wallet::newAccountByMnemonic();
+        var_dump($res2);
+
+        $res3 = Wallet::revertAccountByMnemonic($res2['mnemonic']);
+        var_dump($res3);
+
+        $this->assertEquals($res3['key'], $res2['key']);
+    }
+}
